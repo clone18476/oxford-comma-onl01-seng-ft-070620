9c2b7b22e6b5ffaf5bd4830bb040f005f974bb36
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 17:30:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
-require "pry"
 
 
 def oxford_comma(array)
   if array.length == 1
-    return array[0]
+    return array[0] # Would return first element in array
   elsif array.length == 2
-    return array.join(' and ')
+    return array.join(' and ') # Would return "el1 and el2"
   elsif array.length == 3
     array[-1].insert(0,'and ')
-    return array.join(', ')
+    return array.join(', ') # Would return ""
   else 
     array[-1].insert(0,'and ')
     return array.join(', ')
